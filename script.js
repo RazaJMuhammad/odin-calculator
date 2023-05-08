70aba7363533acee98ad2dca492ce03b617d064e
Raza Muhammad <razacoding@gmail.com>
2023-05-08 23:35:00 +0200

    Add blank Javascript file

---
+++ script.js
@@ -0,0 +1,5 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById('equalsBtn');
+console.log(equalsBtn);
+
