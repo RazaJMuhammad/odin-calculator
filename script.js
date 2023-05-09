ee03ea65aae0b0118b83a05fe294ea1f03eb92c9
Raza Muhammad <razacoding@gmail.com>
2023-05-09 15:18:19 +0200

    Add event listener to number buttons

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operator]");
 const equalsButton = document.getElementById('equalsBtn');
-console.log(equalsBtn);
+const clearButton = document.querySelector(".clear");
+const topScreen = document.querySelector(".screen-last");
+const bottomScreen = document.querySelector(".screen-current");
 
+let currentOperation = '';
+let lastOperation = '';
+
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => showNumbers(button.textContent));
+})
+
+
+  
+
+clearButton.addEventListener('click', clearScreen);
+
+
+
+function showNumbers(textContent) {
+    if (bottomScreen.textContent == '0'){
+        bottomScreen.innerHTML = textContent;
+    }else{
+        bottomScreen.innerHTML += textContent;
+    }
+    
+}
+
+
+function clearScreen() {
+    topScreen.textContent = "0";
+    bottomScreen.textContent = "0";
+    console.log("works");
+} 
